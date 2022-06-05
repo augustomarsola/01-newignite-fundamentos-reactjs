1e70aff5eb4bc1208cfbde8db2c4cd80b872b6cb
Augusto Marsola <augusto.marsola@live.com>
2022-06-04 23:26:32 -0300

    Initiate the post component

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,7 +1,42 @@
+import styles from "./Posts.module.scss";
+
 export function Posts() {
   return (
-    <div>
-      <p>Post</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            src="https://avatars.githubusercontent.com/u/25964446?v=4"
+            alt="Foto do autor da postagem"
+            className={styles.avatar}
+          />
+          <div className={styles.authorInfo}>
+            <strong>Augusto Marsola</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio ás 08:13h" dateTime="2022-05-11 08:10:10">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala pessoal 👏</p>
+
+        <p>
+          Acabei de publicar mais uma contribuição no meu portfólio. Corre lá
+          que está um projeto sensacional.
+        </p>
+
+        <p>
+          👉 <a href="#">super.proj/megaproj</a>
+        </p>
+
+        <p>
+          <a href="#">#novoprojeto #ignite #decolar</a>
+        </p>
+      </div>
+    </article>
   );
 }
