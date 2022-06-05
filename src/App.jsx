aa46ed6f8c542c4fb3907dea37e3ab743163fe3e
Augusto Marsola <augusto.marsola@live.com>
2022-06-04 21:18:04 -0300

    Initializing the header component stylish

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { Header } from "./components/header";
+import "./global.scss";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header />
+      <h1>Hello World</h1>
+    </>
+  );
 }
 
 export default App;
